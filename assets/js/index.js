c88580172973e6577462abbb2267c2912348af0c
danielgonzalez22 <kavellid63@gmail.com>
2022-08-12 01:48:00 -0300

    everyfuckingthing

---
+++ assets/js/index.js
@@ -0,0 +1,91 @@
+const API = fetch("https://apipetshop.herokuapp.com/api/articulos")
+
+let eventosAPI;
+
+
+const datosAPI = async () => {
+    try{
+        const res = await API
+        eventosAPI = await res.json()
+        printCarousel(eventosAPI.response)
+    }catch(err){
+        console.log(err)
+    }
+}
+
+datosAPI()
+
+
+let container1 = document.querySelector('.carousel-inner')
+
+
+function printCarousel(arrayDatos){
+    let primer = true
+    arrayDatos.forEach(item => {
+        if(item.stock < 4 && item.stock > 0){
+            let carouselItem = document.createElement('div')
+            if(primer){
+                carouselItem.className = "carousel-item active"
+                primer = false
+            }else{
+                carouselItem.className = "carousel-item"
+            }
+            carouselItem.innerHTML = `
+                                    <div class="card-wrapper">
+                                        <div class="product-carousel">
+                                            <div class="product-image">
+                                                <img src="${item.imagen}" alt="">
+                                            </div>
+                                            <div class="product-specification pt-3">
+                                                <h4 class="mb-2 pt-1">${item.nombre}</h4>
+                                                <div class="price-tag justify-content-center py-2">
+                                                    <span>$</span>
+                                                    <p class="size-price">${item.precio}</p>
+                                                    <span>ARG</span>
+                                                </div>
+                                                <div class="buy-button justify-content-evenly">
+                                                    <div class="form-group mb-4">
+                                                    <label class="card-input ps-2" for="exampleInputPassword1">cantidad</label>
+                                                        <input type="number" min="1" value="1" max="${item.stock}" class="form-control cantidad-input" style="width: 5rem;">
+                                                    </div>
+                                                    <button class="cardButton mb-1  me-2">Agregar</button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    `
+            container1.appendChild(carouselItem)
+        }
+    });
+}
+
+
+
+
+
+
+
+// ------------------- CARTA CAROUSEL -------------- //
+
+
+
+/* <div class="product-carousel cardCarousel-style">
+<div class="product-image">
+    <img src="https://puppis.vteximg.com.br/arquivos/ids/163598-1000-1000/228767.jpg?v=636746890828270000" alt="">
+</div>
+<div class="product-specification pt-3">
+    <h4 class="mb-2 pt-1">Juguete interactivo IQ Treat Ball</h4>
+    <div class="price-tag justify-content-center py-2">
+        <span>$</span>
+        <p class="size-price">300</p>
+        <span>ARG</span>
+    </div>
+    <div class="buy-button justify-content-evenly">
+        <div class="form-group mb-4">
+        <label class="card-input ps-2" for="exampleInputPassword1">cantidad</label>
+            <input type="number" min="1" value="1" max="${item.stock}" class="form-control cantidad-input" style="width: 5rem;">
+        </div>
+        <button class="cardButton mb-1  me-2">Agregar</button>
+    </div>
+</div>
+</div> */
---
+++ assets/js/juguetes.js
@@ -0,0 +1,152 @@
+
+let dataAPI;
+
+getData()
+async function getData() {
+    await fetch("https://apipetshop.herokuapp.com/api/articulos")
+        .then(response => response.json())
+        .then(data => dataAPI = data.response)
+
+
+    let cardContainerP = document.getElementById('card-container-p');
+    let toyProducts = [];
+
+    toysCardsFilter()
+    function toysCardsFilter() {
+        for (product of dataAPI) {
+            if (product.tipo == "Juguete") {
+                toyProducts.push(product)
+            }
+        }
+
+        return toyProducts;
+    }
+
+
+    toysCards(toyProducts);
+    function toysCards(array) {
+        cardContainerP.innerHTML = ""
+        if (array.length>0){
+        array.forEach(item => {
+            if (item.stock<=3){
+                let cardBody = document.createElement("div");
+                cardBody.innerHTML = `  <div class="product-carousel card-style m-3 product shop-card ">
+                <div class="product-image ">
+                    <img src="${item.imagen}" class=" img-fluid">
+                </div>
+                <span class= "d-flex justify-content-center span-color"> ¡Solo ${item.stock} en stock!</span>
+                <div class="product-specification">
+                    <h4 class="mt-4 pt-3">${item.nombre}</h4>
+                    
+                    <div class="d-flex justify-content-center">
+                        <span class="mt-2">$</span>
+                        <p class="size-price">${item.precio}</p>
+                        <span class="mt-2">ARG</span>
+                    </div>
+                    <div class="buy-button pb-5 d-flex justify-content-evenly">
+                        <div class="form-group mb-4">
+                        <label class="card-input ps-2">Cantidad</label>
+                            <input type="number" min="1" value="1" max="${item.stock}" class="form-control mb-1 cantidad-input" style="width: 5rem;">
+                        </div>
+                        <button class="cardButton mb-1 btn-primary button-class" data-th="${item._id}" me-2">Agregar</button>
+                    </div>
+                </div>`
+                cardContainerP.appendChild(cardBody);
+            } else{
+                let cardBody = document.createElement("div");
+                cardBody.innerHTML = `  <div class="product-carousel card-style m-3 product shop-card ">
+                <div class="product-image ">
+                    <img src="${item.imagen}" class=" img-fluid">
+                </div>
+                <span class= "d-flex justify-content-center span-color-v"> ${item.stock} en stock</span>
+                <div class="product-specification">
+                    <h4 class="mt-4 pt-3">${item.nombre}</h4>
+                    
+                    <div class="d-flex justify-content-center">
+                        <span class="mt-2">$</span>
+                        <p class="size-price">${item.precio}</p>
+                        <span class="mt-2">ARG</span>
+                    </div>
+                    <div class="buy-button pb-5 d-flex justify-content-evenly">
+                        <div class="form-group mb-4">
+                        <label class="card-input ps-2">Cantidad</label>
+                            <input type="number" min="1" value="1" max="${item.stock}" class="form-control mb-1 cantidad-input" style="width: 5rem;">
+                        </div>
+                        <button class="cardButton mb-1 btn-primary button-class" data-th="${item._id}" me-2">Agregar</button>
+                    </div>
+                </div>` 
+            
+                cardContainerP.appendChild(cardBody);
+            }
+        })}else {
+            let cardBody = `  <div class="d-flex flex-wrap justify-content-center"> <div class="product-carousel card-style m-3 product">
+            <div class="product-image ">
+                <img src="https://cdn.dribbble.com/users/9232825/screenshots/18931408/media/d901e116d4292f1dba4a452a5a2718aa.png?compress=1&resize=400x300" class=" img-fluid">
+            </div>
+            <div class="product-specification">
+                <h4 class="mt-5 pt-3">Sin resultados</h4>
+                <div class="buy-button pb-5 d-flex justify-content-evenly">
+                </div>
+            </div></div>`
+        cardContainerP.innerHTML += cardBody;
+    }
+        }
+
+    //carrito
+    let carrito = [];
+
+    if (localStorage.getItem("carrito")) {
+        carrito = JSON.parse(localStorage.getItem("carrito"));
+    }
+    // agregar addeventlistener para filtrar
+    let contenidoSearch = document.querySelector(".search-input");
+    contenidoSearch.addEventListener("keyup", enviarAlCarrito);
+
+    enviarAlCarrito()
+    function enviarAlCarrito() {
+
+        let textSearch = contenidoSearch.value.toLowerCase();
+        let filtrado = filtrarNombre(textSearch)
+        toysCards(filtrado)
+        //filtro
+        document.querySelectorAll('.btn-primary').forEach(element => element.addEventListener('click', function () {
+
+            let cantidad = this.closest('.product').querySelector('.cantidad-input').value;
+            //class que tomo del input
+            let id = this.getAttribute("data-th");
+            // obtiene la id cada vez que clickeo el boton que agrega al carrito
+            let agregoProductos = false; //comienza vacio
+            let producto = {
+                id: id,
+                cantidad: cantidad
+            }
+            if (localStorage.getItem("carrito")) {
+                for (let i = 0; carrito.length > i; i++) {
+                    if (carrito[i].id == id) {
+                        carrito[i].cantidad == Number(carrito[i].cantidad) + Number(cantidad)
+                        // toma del stock la cantidad agregada al carrito.
+                        agregoProductos = true;
+                        console.log(agregoProductos)
+                    }
+                }
+            } if (agregoProductos === false) {
+                carrito.push(producto);
+                //  console.log(producto)
+            }
+            console.log(producto)
+            localStorage.setItem("carrito", JSON.stringify(carrito));
+        }))
+    }
+
+    // llamo funcion de filtro
+    function filtrarNombre(nombre) {
+        let filtrado = toyProducts.filter((producto) => {
+            return producto.nombre.toLowerCase().includes(nombre)
+        })
+        return filtrado
+    }
+
+
+
+}
+
